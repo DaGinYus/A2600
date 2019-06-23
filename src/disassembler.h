8e75caed8c5732e3b267ba1a79b7701822cb26e1
0xFireball <0xFireball@outlook.com>
2019-06-23 14:32:59 -0700

    ok that's enough for now

--- src/disassembler.h
+++ src/disassembler.h
@@ -17,6 +17,66 @@ void disassemble_rom(char* buf, int size) {
             name = "ADC_AB";
             sz = 3;
             break;
+        case ADC_ABX:
+            name = "ADC_ABX";
+            sz = 3;
+            break;
+        case ADC_ABY:
+            name = "ADC_ABY";
+            sz = 3;
+            break;
+        case ADC_IMM:
+            name = "ADC_IMM";
+            sz = 2;
+            break;
+        case ADC_INX:
+            name = "ADC_INX";
+            sz = 2;
+            break;
+        case ADC_INY:
+            name = "ADC_INY";
+            sz = 2;
+            break;
+        case ADC_ZP:
+            name = "ADC_ZP";
+            sz = 2;
+            break;
+        case ADC_ZPX:
+            name = "ADC_ZPX";
+            sz = 2;
+            break;
+        case AND_AB:
+            name = "AND_AB";
+            sz = 3;
+            break;
+        case AND_ABX:
+            name = "AND_ABX";
+            sz = 3;
+            break;
+        case AND_ABY:
+            name = "AND_ABY";
+            sz = 3;
+            break;
+        case AND_IMM:
+            name = "AND_IMM";
+            sz = 2;
+            break;
+        case AND_INX:
+            name = "AND_INX";
+            sz = 2;
+            break;
+        case AND_INY:
+            name = "AND_INY";
+            sz = 2;
+            break;
+        case AND_ZP:
+            name = "AND_ZP";
+            sz = 2;
+            break;
+        case AND_ZPX:
+            name = "AND_ZPX";
+            sz = 2;
+            break;
         case JMP_AB:
             name = "JMP_AB";
             sz = 3;
@@ -32,7 +92,8 @@ void disassemble_rom(char* buf, int size) {
         std::cout << name;
         for (uint8_t i = 1; i < sz; i++) {
             uint8_t v = buf[p + i];
-            std::cout << " " << "0x" << std::setfill('0') << std::setw(2) << unsigned(v);
+            std::cout << " "
+                      << "0x" << std::setfill('0') << std::setw(2) << unsigned(v);
         }
         std::cout << std::endl;
         p += sz;
